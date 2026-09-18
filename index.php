f33806aed70faf9df3fad1a4377fdbbf0d756644
Phillip Ruiru <phillipruiru@gmail.com>
2026-09-18 18:08:30 +0300

    converted .html to .php and modified the css

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Motion Wreck Studio In Nairobi</title>
+    <link rel="stylesheet" href="CSS/style.css">
+
+</head>
+
+    <body>
+
+        <?php include 'nav.php'; ?>
+
+        <main>
+
+        <section id="hero">
+
+            <h1>Moments, Softly told.</h1>
+            <h2>We turn your biggest days into images you'll never stop looking at.</h2>
+
+        </section>
+
+        </main>
+        
+    </body>
+
+
+</html>
---
+++ nav.php
@@ -0,0 +1,42 @@
+
+<nav>
+    <div class="logo">
+        <img src="assets/motion-wreck-logo.svg" alt="logo">
+    </div>
+
+    <ul class="mega-space">
+        <li>
+            <a href="#" id="home-link">Motion Wreck Studio</a>
+        </li>
+
+        <li>
+            <a href="#">About Us</a>
+        </li>
+
+        <li class="mega">
+            <a href="#">Gallary <span class="arrow">˅</span></a>
+
+            <div class="mega-menu">
+                <a href="#">Birthday</a>
+                <a href="#">Passport</a>
+                <a href="#">Weddings</a>
+                <a href="#">Events</a>
+                <a href="#">Baby Bump</a>
+                <a href="#">Corporates</a>
+                <a href="#">Family Shoots</a>
+                <a href="#">Graduation</a>
+            </div>
+        </li>
+
+        <li>
+            <a href="#">Gowns for Hire</a>
+        </li>
+
+        <li>
+            <a href="#">Contact Us</a>
+        </li>
+
+    </ul>
+
+</nav>
+
